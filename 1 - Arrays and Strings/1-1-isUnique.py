55b78fe14a872cb59507a1c1bbfba530f4f60677
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-06-13 18:25:22 -0500

    Added solutions

--- 1 - Arrays and Strings/1-1-isUnique.py
+++ 1 - Arrays and Strings/1-1-isUnique.py
@@ -1,6 +1,6 @@
-# Time: O(n)
-# Space: O(1)
-def isUnique(s):
+def isUniqueCharArray(s):
+    # Time: O(n)
+    # Space: O(1)
     if len(s) == 0:
         return True
     if len(s) > 128:
@@ -13,5 +13,31 @@ def isUnique(s):
         charArray[asc] = True
     return True
 
+
+def isUniqueBruteForce(s):
+    # Time: O(n2)
+    # Space: O(1)
+    if len(s) == 0:
+        return True
+    for i in range(len(s)):
+        for j in range(len(s)):
+            if i!=j and s[i]==s[j]:
+                return False
+    return True
+
+
+def isUniqueSort(s):
+    # Time: O(nlogn)
+    # Space: depends on the sort method
+    if len(s)==0:
+        return True
+    newString = ''.join(sorted(s))
+    for i in range(len(s)-1):
+        if newString[i]==newString[i+1]:
+            return False
+    return True
+
 word = "Cracking The Coding Interview"
-print(isUnique(word))
+print(isUniqueCharArray(word))
+print(isUniqueBruteForce(word))
+print(isUniqueSort(word))
