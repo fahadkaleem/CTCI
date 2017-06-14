cead895495afe699e63fcf021ba82af69267811e
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-06-13 19:29:07 -0500

    Added solutions

---
+++ 1 - Arrays and Strings/1-6-stringComression.py
@@ -0,0 +1,15 @@
+def stringCompression(word):
+    # Time: O(n)
+    letterCount = 1
+    output = ''
+    for i in range(len(word)-1):
+        if word[i]==word[i+1]:
+            letterCount+=1
+        else:
+            output+=word[i]+ str(letterCount)
+            letterCount = 1
+    output+=word[-1]+str(letterCount)
+    return output
+
+print(stringCompression("abcdef"))
+
