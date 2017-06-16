95573741b6d0987fcaa0ab1190cd4fb851579ac0
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-06-16 12:39:38 -0500

    Added solutions

---
+++ LinkedList/2-2-kthFromLast.py
@@ -0,0 +1,19 @@
+from LinkedList import LinkedList
+
+# Time: O(n)
+def kth_to_last(linked_list, k):
+    slow = fast = linked_list.head
+    for i in range(k):
+        if fast is None:
+            return None
+        fast = fast.nextNode
+
+    while fast:
+        slow = slow.nextNode
+        fast = fast.nextNode
+    return slow.data
+
+linked_list = LinkedList()
+linked_list.__generate__(10,0,9)
+linked_list.__print__()
+print(kth_to_last(linked_list,3))
